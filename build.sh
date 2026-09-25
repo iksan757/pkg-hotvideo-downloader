10e1b332aa9827c64b167577769cbdeaee824d9b
tuan_muda <rumasoreng757@gmail.com>
2026-09-25 12:05:04 +0700

    Remove unnecessary newline in build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 TERMUX_PKG_NAME=hotvideo-downloader
 TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_SHA256=4f577a3d213d2440ebffe2c682106d59ceefc95eec7f22337435dc167396cb6d
-
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_SRCURL=https://github.com/iksan757/pkg-hotvideo-downloader/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
 TERMUX_PKG_DEPENDS="python python-yt-dlp yt-dlp-ejs aria2 ffmpeg"
